bfdd79bd0a61d9b3e5505b9c2e5f23941ebd2524
fyodor-rs <wg189807@163.com>
2022-05-06 17:00:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+var mouseEvents = require("global-mouse-events")
+var robot = require("robotjs")
+var t;
+var o = false
+
+var GlobalKeyboardListener = require('node-global-key-listener')
+var v = new GlobalKeyboardListener.GlobalKeyboardListener();
+v.addListener(function (e, down) {
+    if (e.name === 'HOME' && e.state === 'UP') {
+        o = !o
+    }
+    if (e.name === 'END') {
+        process.exit(1)
+    }
+});
+
+mouseEvents.on("mousedown", data => {
+    if (data.button === 2) {
+        o = true
+    }
+    if (o && data.button === 1) {
+        t = setInterval(function () {
+            robot.moveMouseRelative(0, 5)
+        }, 1)
+    }
+});
+
+mouseEvents.on("mouseup", data => {
+    if (data.button === 2) {
+        o = false
+    }
+    clearInterval(t)
+});                                                                                                                 
